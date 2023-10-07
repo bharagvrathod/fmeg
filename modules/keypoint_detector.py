de4d4ea80b8a41f52552a5fb4619f092fc127040
bharagvrathod <bhargav.rathod01@gmail.com>
2023-10-07 12:51:54 +0800

    changes made to forward function matching heat map and hessian map 2

--- modules/keypoint_detector.py
+++ modules/keypoint_detector.py
@@ -65,7 +65,7 @@ class KPDetector(nn.Module):
 
             # Ensure heatmap and hessian_map have compatible dimensions
             heatmap = heatmap.unsqueeze(2)
-            heatmap = heatmap.expand(-1, -1, self.num_hessian_maps, -1)  # Match dimensions
+            heatmap = heatmap.unsqueeze(3)  # Add an extra dimension to match hessian_map
             hessian = heatmap * hessian_map
             hessian = hessian.view(final_shape[0], final_shape[1], 4, -1)
 
