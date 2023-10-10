4b175ca0d9dd0c0d3489aa22a419508bb79455d9
bharagvrathod <bhargav.rathod01@gmail.com>
2023-10-10 19:51:27 +0800

    Revert "changing forward in kp detector without self.jacobian"
    This reverts commit 0967a55e23b6a032b087f47be023d1921467b610.

--- modules/keypoint_detector.py
+++ modules/keypoint_detector.py
@@ -1,15 +1,23 @@
+from torch import nn
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from modules.util import Hourglass, make_coordinate_grid, AntiAliasInterpolation2d
 
 class KPDetector(nn.Module):
-    def __init__(self, block_expansion, num_kp, num_channels, max_features, num_blocks, temperature, estimate_hessian=False, scale_factor=1, single_hessian_map=False, pad=0):
+    """
+    Detecting a keypoints. Return keypoint position and Hessian near each keypoint.
+    """
+
+    def __init__(self, block_expansion, num_kp, num_channels, max_features,
+                 num_blocks, temperature, estimate_hessian=False, scale_factor=1,
+                 single_hessian_map=False, pad=0):
         super(KPDetector, self).__init__()
 
-        self.predictor = Hourglass(block_expansion, in_features=num_channels, max_features=max_features, num_blocks=num_blocks)
+        self.predictor = Hourglass(block_expansion, in_features=num_channels,
+                                   max_features=max_features, num_blocks=num_blocks)
 
-        self.kp = nn.Conv2d(in_channels=self.predictor.out_filters, out_channels=num_kp, kernel_size=(7, 7), padding=pad)
+        self.kp = nn.Conv2d(in_channels=self.predictor.out_filters, out_channels=num_kp, kernel_size=(7, 7),
+                            padding=pad)
 
         if estimate_hessian:
             self.num_hessian_maps = 1 if single_hessian_map else num_kp
@@ -26,11 +34,15 @@ class KPDetector(nn.Module):
             self.down = AntiAliasInterpolation2d(num_channels, self.scale_factor)
 
     def gaussian2kp(self, heatmap):
+        """
+        Extract the mean and from a heatmap
+        """
         shape = heatmap.shape
         heatmap = heatmap.unsqueeze(-1)
         grid = make_coordinate_grid(shape[2:], heatmap.type()).unsqueeze_(0).unsqueeze_(0)
         value = (heatmap * grid).sum(dim=(2, 3))
         kp = {'value': value}
+
         return kp
 
     def forward(self, x):
@@ -48,6 +60,7 @@ class KPDetector(nn.Module):
         out = self.gaussian2kp(heatmap)
 
         if self.hessian is not None:
+            # Assuming self.hessian contains the second-order derivatives
             hessian_map = self.hessian(feature_map)
             hessian_map = hessian_map.reshape(final_shape[0], self.num_hessian_maps, 4, final_shape[2], final_shape[3])
             heatmap = heatmap.unsqueeze(2)
@@ -59,3 +72,11 @@ class KPDetector(nn.Module):
             out['hessian'] = hessian
 
         return out
+
+
+
+
+
+
+
+
